6dc13d2d4f113264fdd76e84b014c69bf5ec9a64
FernandoVelazco <daniel.fernando.velazco@gmail.com>
2025-05-02 22:02:08 -0500

    test 02-JS-1 / 012 esImpar

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -144,6 +144,12 @@ function esImpar(num) {
   // De lo contrario, devuelve "false"
   // Tu código:
 
+  if (num % 2 !== 0) {
+    return true;
+  } else {
+    return false;
+  }
+
 }
 
 function elevarAlCuadrado(num) {
