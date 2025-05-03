0134013889fa65d46b48a487b98cd339848c7c86
FernandoVelazco <daniel.fernando.velazco@gmail.com>
2025-05-02 21:28:51 -0500

    test 02-JS-1 / 007 tienenMismaLongitud

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -84,6 +84,12 @@ function tienenMismaLongitud(str1, str2) {
   // De lo contrario, devuelve "false"
   // Tu código:
 
+  if (str1.length === str2.length) {
+    return true;
+  } else {
+    return false;
+  }
+
 }
 
 function menosQueNoventa(num) {
